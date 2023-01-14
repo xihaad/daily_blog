d0115db7ba269aa439e7acc53c28336ce30a2f96
xihaad <zihad.rafi16@gmail.com>
2023-01-15 00:27:07 +0600

    blog edited

--- lib/screens/blog.dart
+++ lib/screens/blog.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
+
+
   @override
   State<Home> createState() => _HomeState();
 }
