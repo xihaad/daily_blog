840598b243183258081fa3e5653f6548132b68e0
xihaad <zihad.rafi16@gmail.com>
2023-01-15 00:30:13 +0600

    profile edited

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,6 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+
 import 'package:daily_blog/main.dart';
 
 void main() {
@@ -19,7 +20,7 @@ void main() {
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
+
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
